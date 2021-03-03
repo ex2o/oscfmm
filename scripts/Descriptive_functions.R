bb83b5a7a6e73599998e250891d85c55a479274e
Daniel Vidali Fryer <daniel-fryer@live.com.au>
2021-03-03 21:52:08 +1100

    prying required data

--- scripts/Descriptive_functions.R
+++ scripts/Descriptive_functions.R
@@ -1,5 +1,77 @@
 # Summary functions -------------------------------------------------------
 
+# logical vector of whether each msid in a list of non-unique msids 
+# appears in the top k number of times in that list
+is_in_top_k <- function(msids, k) {
+  msidt <- table(msids)
+  best_msids <- as.integer(names(sort(msidt, dec = T)[1:k]))
+  msids %in% best_msids
+}
+
+compute_test_runs_to_add <- function(res, min_test_runs = 10) {
+  resr <- count_test_runs_per_draw(res, min_test_runs = min_test_runs)
+  resr_low <- resr$n[resr$n < min_test_runs]
+  return(length(resr_low)*min_test_runs - sum(resr_low))
+}
+
+discard_draws_and_compute_required_runs <- function(
+  res, min_test_runs = 10, 
+  min_draws = 16, 
+  target_num_hypers = 144) {
+  
+  resd <- discard_draws_with_few_test_runs(res, few = min_test_runs)
+  # Check number of mixture draws per hyperparameter set
+  rdh <- count_draws_per_hyper(resd, min_draws = min_draws)
+  rdh_low <- rdh[rdh$n < min_draws,]
+  target <- nrow(rdh_low)*min_draws 
+  target <- target + (target_num_hypers-count_unique_hypers(resd))*min_draws
+  actual <- sum(rdh_low$n)
+  message("REQUIRE: ", target-actual, 
+      " new mixture draws with ",min_test_runs,
+      " test runs each ")
+  return((target-actual)*min_test_runs)
+}
+
+count_unique_hypers <- function(res, target_num_hypers = 144) {
+   ruh <- group_by(res, NN, LL, DD, BO, TrueG, PiLow) %>% 
+     summarise()
+   n_hypers <- nrow(ruh)
+   n_missing_hypers <- target_num_hypers - n_hypers
+   if (n_missing_hypers > 0) {
+     message("There are ", n_missing_hypers, " missing hypers") 
+   }
+   return(n_hypers)
+}
+
+# Check number of test runs per mixture draw
+count_test_runs_per_draw <- function(res, min_test_runs = 10) {
+  rpd <-  group_by(res, msid) %>% 
+    summarise(n=n())
+  message("ratio of draws with < ",min_test_runs," test runs = ", 
+          round(nrow(rpd[rpd$n < min_test_runs,])/nrow(rpd),2))
+  return(return(rpd))
+}
+
+# Check number of mixture draws per hyperparameter set
+count_draws_per_hyper <- function(res, min_draws = 16) {
+  dph <- group_by(res, NN, LL, DD,BO, TrueG, PiLow) %>% 
+    summarise(n=length(unique(msid)))
+  message("ratio of hypers with < ", min_draws," draws = ", 
+          round(nrow(dph[dph$n < min_draws,])/nrow(dph),2))
+  return(dph)
+}
+
+# Remove the mixture draws with less than 10 test runs
+discard_draws_with_few_test_runs <- function(res, few = 10) {
+  rpd <- count_test_runs_per_draw(res, min_test_runs = few)
+  bad_msids <- rpd[rpd$n < few,]$msid
+  resd <- res[!(res$msid %in% bad_msids),]
+  count_unique_hypers(resd)
+  return(resd)
+}
+
+
+
 # return logical vector indicating if each result errored out
 errored <- function(results) {
   
@@ -46,24 +118,30 @@ add_quantiles <- function(results, alpha=0.1) {
   res
 }
 
-# The function takes an input vector of paths to multiple
-# result files in rds format, and combines their cleaned
-# first_accepts tables
-
-read_clean_and_combine_first_accepts <- function(results_list) { 
-  
+remove_errors <- function(results_list) {
   # Remove errors and check error rates
   results <- lapply(results_list, function(x){
     errd <- errored(x)
     cat("Errored=",sum(errd)/length(x),"\n")
     x[!errd]
   })
+  results
+}
+
+
+
+# The function takes an input vector of paths to multiple
+# result files in rds format, and combines their cleaned
+# first_accepts tables
+
+clean_and_combine_first_accepts <- function(results_list) { 
   
   # combine first accepts into a tables
-  res <- lapply(results, combine_first_accepts)
+  res <- lapply(results_list, combine_first_accepts)
   
   # extract ms_draw ids and add to the tables
-  msid <- lapply(results, ms_draw_ids)
+  msid <- lapply(results_list, ms_draw_ids)
+   
   for (i in 1:length(res)) {
     res[[i]] <- data.frame(res[[i]], msid = msid[[i]])
     cat("Number of ms_draws=",
@@ -90,9 +168,19 @@ read_clean_and_combine_first_accepts <- function(results_list) {
 # Give a unique ID to each unique ms_draw in the results
 
 ms_draw_ids <- function(results) {
-  
+
   # Using just the first element of the Mu matrix as a unique id for an ms_draw
-  as.integer((
-    unlist(lapply(results, function(x){attr(x,"sim_para")$Mu[1,1]}))
-  )*1000000000)
+  unlist(lapply(results, function(x){make_msid(attr(x,"sim_para"))}))
+}
+
+# ms_draw_ids <- function(results) {
+# 
+#   # Using just the first element of the Mu matrix as a unique id for an ms_draw
+#   as.integer((
+#     unlist(lapply(results, function(x){attr(x,"sim_para")$Mu[1,1]}))
+#   )*1000000000)
+# }
+
+make_msid <- function(sim_para) {
+  as.integer(sim_para$Mu[1,1]*1000000000)
 }
--- scripts/Descriptives.R
+++ scripts/Descriptives.R
@@ -23,9 +23,14 @@ peek(results_list)
 
 #undebug(read_clean_and_combine_first_accepts)
 
-res <- read_clean_and_combine_first_accepts(results_list)
+results_list <- remove_errors(results_list)
+res <- clean_and_combine_first_accepts(results_list)
 
+#write.csv(res, file = "preliminary_results.csv")
 head(res)
+
+
+# DECIDING ON WHICH NEW DATA TO ADD --------------------------------------
 
 # Table of sample sizes in each parameter combination
 t <- table(dplyr::select(res, !starts_with("p"), -msid)); t
@@ -39,45 +44,82 @@ ds_grid <- dplyr::select(tdf_low, c(NN, LL))
 ms_grid$eps <- 0.001
 ms_grid
 ds_grid
-saveRDS(ms_grid, "ms_grid.rds")
-saveRDS(ds_grid, "ds_grid.rds")
+# saveRDS(ms_grid, "ms_grid.rds")
+# saveRDS(ds_grid, "ds_grid.rds")
 
+min_draws <- 16
+min_test_runs <- 10
+num_hypers <- 144
+
+# Count the number of unique sets of hyperparameters
+count_unique_hypers(res, target_num_hypers = num_hypers)
+
+# Check number of test runs per mixture draw
+count_test_runs_per_draw(res, min_test_runs = min_test_runs)
+
+# Check number of mixture draws per hyperparameter set
+count_draws_per_hyper(res, min_draws = min_draws)
+
+# If we remove the mixture draws with less than min_test_runs test runs,
+# then we need this many more test runs
+discard_draws_and_compute_required_runs(
+  res, min_test_runs = min_test_runs, min_draws = min_draws, 
+  target_num_hypers = num_hypers)
+
+# If, on the other hand, we add test runs to the existing mixture draws
+# so that every mixture draw has at least min_test_runs, but only after
+# restricting our attention to the top k=min_draws draws with the most test runs,
+# then we only need to make this many more test runs:
+restk <- group_by(res, NN, LL, DD, BO, TrueG, PiLow) %>% 
+  filter(is_in_top_k(msid, k = min_draws))
+compute_test_runs_to_add(restk, min_test_runs = min_test_runs)
+
+
+# LOOKING AT SIGNIFICANCE LEVELS ------------------------------------------
 
 # Check the alpha_p3 (significance) for each msid
 resg <- group_by(res, msid, TrueG)
 msg <- summarize(resg
-                 ,alpha_p1 = mean(p1 > 5) 
-                 ,alpha_p2 = mean(p2 > 5)
-                 ,alpha_p3 = mean(p3 > 5)
+                 ,alpha_p1 = mean(p1 > TrueG) 
+                 ,alpha_p2 = mean(p2 > TrueG)
+                 ,alpha_p3 = mean(p3 > TrueG)
                  ,n = n()
 )
 msg
 plot(msg$n, msg$alpha_p1, main = "alpha_p3 by mixture draw sample size")
 hist(msg$alpha_p3, main = "alpha_p3 for each mixture draw", breaks=200)
 
+
 # obtain msids above a certain alpha_3 cutoff point
 good_msids <- filter(msg, alpha_p3 < 0.2)$msid
 
 # Check that there are no bad msids
 check <- length(good_msids) == nrow(msg)
-if (!check) warning("There are bad msids")
+if (!check) warning("There are ", nrow(msg)-length(good_msids)," bad msids ",
+                    "ratio = ", 1-length(good_msids)/ nrow(msg))
 
 # uncomment the RHS to filter only good msids 
 resf <- filter(res, msid %in% good_msids)
 
 # Group by unique parameter combination for summary stats
-resg <- group_by(resf, across(!starts_with(c("p","msid"))))
-summarize(resg
+resg <- group_by(resf, NN, LL, DD, BO, TrueG, PiLow)
+resgs <- summarize(resg
          ,n = n() 
          ,mu_p1 = mean(p1) 
          ,mu_p2 = mean(p2) 
          ,mu_p3 = mean(p3)
-         ,alpha_p1 = mean(p1 > 5) 
-         ,alpha_p2 = mean(p2 > 5)
-         ,alpha_p3 = mean(p3 > 5)
+         ,alpha_p1 = mean(p1 > TrueG) 
+         ,alpha_p2 = mean(p2 > TrueG)
+         ,alpha_p3 = mean(p3 > TrueG)
          ,ms_draws = length(unique(msid))
-) %>% filter(LL == 2) %>% 
-  select(!starts_with("mu"), -n, -TrueG, -ms_draws)
+) 
+cat("There are",nrow(resgs),"hyperparameter sets")
+
+#%>% filter(LL == 2) %>% 
+#  select(!starts_with("mu"), -n, -TrueG, -ms_draws)
+
+cat("These are the alphas over 0.05:")
+resgs$alpha_p3[resgs$alpha_p3 > 0.05]
 
 # Subset the results
 res1 <- resf %>% filter(
@@ -93,6 +135,56 @@ nrow(res1)
 
 # Look at subset size ratio
 nrow(res1)/nrow(resf)
+
+# EXPLORING THE BAD MSIDS -------------------------------------------------
+
+bad_msids <- (group_by(res, msid, TrueG) %>% 
+  summarize(alpha_p3 = mean(p3 > TrueG)) %>% 
+  filter(alpha_p3 >= 0.5))$msid
+length(bad_msids)
+
+# extract the mixture parameters from the results list if they belong
+# to the bad_msids.
+
+all_sim_paras <- unlist(lapply(results_list, function(xl) {
+  lapply(xl, function(x){
+    attr(x,"sim_para")  
+  })
+}), recursive=F)
+
+all_msids <- unlist(lapply(all_sim_paras, make_msid), use.names=F)
+
+is_bsp <- all_msids %in% bad_msids
+bad_sim_paras <- unique(all_sim_paras[is_bsp])
+good_sim_paras <- unique(all_sim_paras[!is_bsp])
+
+length(bad_sim_paras) / (length(bad_sim_paras) + length(good_sim_paras))
+
+get_dets <- function(sim_para) {
+  ns <- dim(sim_para$S)[3]
+  dets <- vector(mode="numeric",length=ns)
+  for( i in 1:ns ) {
+    S <- sim_para$S[,,i]
+    dets[i] <- determinant(S)$modulus
+  }
+  dets
+}
+
+bad_dets <- unlist(lapply(bad_sim_paras, get_dets))
+good_dets <- unlist(lapply(good_sim_paras, get_dets))
+
+plot(good_dets)
+points(bad_dets, col="red")
+
+hist(good_dets)
+hist(bad_dets)
+
+determinant(spb$S[,,1])
+MixSim::pdplot(spb$Pi, spb$Mu, spb$S)
+
+spg <- good_sim_paras[[1]]
+spg$S
+MixSim::pdplot(spg$Pi, spg$Mu, spg$S)
 
 
 # Plot 1 ------------------------------------------------------------------
