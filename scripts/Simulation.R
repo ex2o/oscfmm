610cc98773091e03dcf6739a3521a8cf4b99259d
Daniel Vidali Fryer <danielvfryer@gmail.com>
2022-07-08 21:05:00 +1000

    add loglik only simulation section

--- scripts/Simulation.R
+++ scripts/Simulation.R
@@ -10,6 +10,12 @@ source("Big_test_arma_3.R")
 # Seed
 # set.seed(4200) 
 
+
+# Create results directory --------------------------------------
+mainDir <- "."
+temp_results <- "temp_results"
+dir.create(file.path(mainDir, temp_results), showWarnings = FALSE)
+
 # A grid of simulation parameters
 # params <- expand.grid(NN = c(300,500,1000,2000,5000,10000)
 #             ,TrueG = c(5,10)
@@ -53,12 +59,6 @@ params$GGmax <- params$TrueG + params$GGextra
 params$GGextra <- NULL
 
 
-# Create results directory --------------------------------------
-mainDir <- "."
-temp_results <- "temp_results"
-dir.create(file.path(mainDir, temp_results), showWarnings = FALSE)
-
-
 # Run simulations ---------------------------------------------------------
 results_list <- list()
 time_list <- list()
@@ -76,6 +76,70 @@ for (i in 1:npar) {
 }
 saveRDS(results_list, paste0(temp_results, "/",
   "all_results",Sys.getenv('SLURM_ARRAY_TASK_ID'),".rds"))
+
+
+# Run simulations for loglik only -----------------------------------------
+
+source("AIC_BIC_arma_3.R")
+
+# A grid of simulation parameters for loglik only
+# params <- expand.grid(NN = c(1000,2000,5000,10000)
+#             ,TrueG = c(5,10)
+#             ,LL = c(1,2)
+#             ,DD = c(2,4)
+#             ,BO = c(0.01,0.05,0.1)
+#             ,NumSim = 100
+#             ,GGextra = 2)
+params <- expand.grid(NN = c(1000, 2000)
+                      ,TrueG = c(5)
+                      ,LL = c(1,2)
+                      ,DD = c(2)
+                      ,BO = c(0.01)
+                      ,NumSim = 2
+                      ,GGextra = 1)
+nrow(params)
+head(params, n=16)
+
+make_indices <- function(params) {
+  
+  sat_id <- Sys.getenv('SLURM_ARRAY_TASK_ID')
+  if (sat_id != "") {
+    # Dividing the parameters into sets of 16 so that NN, TrueG and LL are
+    # balanced, means we have 96/16 = 6 runs, that each run should take
+    # about 26 hours. Provide 30 hours for each run in SLURM.
+    index_set <- split(1:nrow(params), rep(1:6, each=16))
+    
+    sat_id <- as.integer(sat_id)
+    indices <- index_set[[sat_id]]
+  } else {
+    indices <- 1:nrow(params) 
+  }
+  return(indices)
+}
+
+indices <- make_indices(params)
+
+params <- params[indices,]
+
+params$GGmax <- params$TrueG + params$GGextra
+params$GGextra <- NULL
+
+results_list <- list()
+time_list <- list()
+npar <- nrow(params)
+for (i in 1:npar) {
+  par <- params[i,]
+  t <- system.time({
+    results_i <- do.call(loglik_arma3, par)
+    results_list[[i]] <- results_i
+  })
+  time_list[[i]] <- t
+  filename <- paste0(temp_results,"/","loglik_",paste0(names(par),"=",par,"-", collapse = ""),".rds")
+  saveRDS(structure(results_i,t=t), filename)
+  cat(paste0("completed params[",i,",] in time t=",round(t[3],1),"s. Remaining params=",npar-i,"\n"))
+}
+saveRDS(results_list, paste0(temp_results, "/",
+                             "loglik_all_results",Sys.getenv('SLURM_ARRAY_TASK_ID'),".rds"))
 
 # estimate_full_time <- function(t) {
 #   # Df <- 2
